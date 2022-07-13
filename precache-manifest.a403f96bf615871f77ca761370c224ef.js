63e47c0d25fa9a9c4a0d53335a34844efc70e08a
Ckauark <ckauark@gmail.com>
2022-07-13 12:20:30 -0300

    Updates

---
+++ precache-manifest.a403f96bf615871f77ca761370c224ef.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c2a04bd87ea78232b1d597dd7b61533",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "a144dee9bb36242ac62c",
+    "url": "/react-gh-pages/static/css/main.1b3e03eb.chunk.css"
+  },
+  {
+    "revision": "b9fc07e2f8d5127d1461",
+    "url": "/react-gh-pages/static/js/2.88c39c1b.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/react-gh-pages/static/js/2.88c39c1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a144dee9bb36242ac62c",
+    "url": "/react-gh-pages/static/js/main.a5536d22.chunk.js"
+  },
+  {
+    "revision": "533a9a6796a2725e92bb",
+    "url": "/react-gh-pages/static/js/runtime-main.4554fbf3.js"
+  },
+  {
+    "revision": "1e39fcc30794c0b0430edc852b2de7e6",
+    "url": "/react-gh-pages/static/media/blur.1e39fcc3.svg"
+  },
+  {
+    "revision": "04a8e1fdcbb4502e0e5931080437d963",
+    "url": "/react-gh-pages/static/media/clock-danger.04a8e1fd.svg"
+  },
+  {
+    "revision": "1c1d5a058e48c36e714b90744a05b7e9",
+    "url": "/react-gh-pages/static/media/clock.1c1d5a05.svg"
+  },
+  {
+    "revision": "ccb86fcd3cbcd993333567f11430da51",
+    "url": "/react-gh-pages/static/media/codes.ccb86fcd.svg"
+  },
+  {
+    "revision": "61ae0a25adb843d74d5484b2f66c6d08",
+    "url": "/react-gh-pages/static/media/happy-feedback.61ae0a25.svg"
+  },
+  {
+    "revision": "35de1fda81d57ff9fcdbdcc52e399b10",
+    "url": "/react-gh-pages/static/media/sad-feedback.35de1fda.svg"
+  },
+  {
+    "revision": "69ffdcea4b13e4b010f2cc268239c078",
+    "url": "/react-gh-pages/static/media/trivia-neon.69ffdcea.svg"
+  },
+  {
+    "revision": "21bade0f2203cf0ed5ba88c102d20b8c",
+    "url": "/react-gh-pages/static/media/trophy.21bade0f.svg"
+  }
+]);
